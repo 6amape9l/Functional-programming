6934532cc624084b65327e5aae4ea2da369e3259
Тимур <tima18152@gmail.com>
2024-03-01 13:51:54 +0300

    Добавлен комментарий для бытрого подключения и предикат daughter(X, Y) "X - дочка Y"

--- Russian_family_tree.pl
+++ Russian_family_tree.pl
@@ -1,3 +1,6 @@
+
+%consult("Russian_family_tree").
+
 man(voeneg).
 man(voe).
 man(ratibor).
@@ -49,7 +52,6 @@ parent(duhovlad,zlatomir).
 parent(zhdana,zdislava).
 parent(zhdana,zlatomir).
 
-
 men():- man(X), print(X), nl, fail.
 women():- woman(X), print(X), nl, fail.
 children(X):- parent(X,Y), print(Y), nl, fail.
@@ -62,3 +64,6 @@ brother(X):- brother(X, Y), X\=Y, print(Y), nl, fail.
 
 b_s(X,Y):- man(X), woman(Y), parent(Z,X), parent(Z, Y), man(Z).
 b_s(X):- b_s(X, Y), X\=Y, print(Y), nl, fail. 
+
+daughter(X, Y):- woman(X), parent(Y, X).
+daughter(X):- daughter(Y, X), print(Y), nl, fail.
