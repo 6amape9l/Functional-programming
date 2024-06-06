55494fb5ab5ba91523629cd06eacc1e7766540c1
Тимур <tima18152@gmail.com>
2024-06-06 03:09:19 +0300

    Создание пробной базы данных

---
+++ database.pl
@@ -0,0 +1,26 @@
+?- pack_install(http).
+?- pack_install(http_json).
+
+:- dynamic department/40.
+
+%Департаменты
+department('Sales', 1),
+department('Marketing', 2),
+department('IT', 3),
+department('HR', 4),
+department('Finance', 5),
+department('Operations', 6),
+department('Customer Service', 7),
+department('Research and Development', 8),
+department('Quality Assurance', 9),
+department('Data Analysis', 10),
+department('Business Development', 11),
+department('Product Management', 12),
+department('Software Engineering', 13),
+department('Network Administration', 14),
+department('Database Administration', 15),
+department('Security', 16),
+department('IT Support', 17),
+department('Web Development', 18),
+department('Mobile Development', 19),
+department('Artificial Intelligence', 20);
